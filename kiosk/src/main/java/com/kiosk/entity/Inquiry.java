d7b6d44949bdf4950505c0afc787d29fbd825c78
JsOoOng <qkr030503@naver.com>
2026-07-08 14:21:02 +0900

    패키지 구조 최종

--- kiosk/src/main/java/com/kiosk/entity/Inquiry.java
+++ kiosk/src/main/java/com/kiosk/entity/Inquiry.java
@@ -1,12 +1,28 @@
 package com.kiosk.entity;
 
-import com.kiosk.entity.enums.InquiryStatus;
-import com.kiosk.entity.enums.InquiryType;
-import jakarta.persistence.*;
-import lombok.*;
+import java.time.LocalDateTime;
+
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDateTime;
+import com.kiosk.entity.enums.InquiryStatus;
+import com.kiosk.entity.enums.InquiryType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "INQUIRIES")
