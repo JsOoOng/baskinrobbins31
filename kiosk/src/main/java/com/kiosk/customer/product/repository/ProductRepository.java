ded78f5908308cf6f8f00be6d9c1a49dc27ddd93
leeyg0212-ops <leeyg0212@gmail.com>
2026-07-10 09:35:36 +0900

    잔 수정

--- kiosk/src/main/java/com/kiosk/customer/product/repository/ProductRepository.java
+++ kiosk/src/main/java/com/kiosk/customer/product/repository/ProductRepository.java
@@ -15,9 +15,18 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     // 특정 지점의 특정 카테고리에 속한 상품 리스트와 해당 지점의 품절 여부를 함께 조회
     // (이 결과를 가지고 앞서 만든 ProductListResponse DTO로 변환하게 됩니다.)
-    @Query("SELECT p, sp.isSoldOut FROM Product p JOIN StoreProduct sp ON p.productId = sp.productId " +
-           "WHERE sp.storeId = :storeId AND p.categoryId = :categoryId AND p.isDisplay = true")
-    List<Object[]> findProductsWithSoldOutStatus(@Param("storeId") Long storeId, @Param("categoryId") Long categoryId);
-
+	@Query("""
+		    SELECT p, sp.isSoldOut
+		    FROM Product p
+		    JOIN StoreProduct sp
+		    ON p.id = sp.product.id
+		    WHERE sp.store.id = :storeId
+		    AND p.category.id = :categoryId
+		    AND p.isDisplay = true
+		""")
+		List<Object[]> findProductsWithSoldOutStatus(
+		        @Param("storeId") Long storeId,
+		        @Param("categoryId") Long categoryId
+		);
 	Optional<Product> findById(Long valueOf);
 }
