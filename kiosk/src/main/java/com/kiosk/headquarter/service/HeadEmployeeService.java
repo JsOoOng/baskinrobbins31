c6b573b409ec7760dc215e19a15f0bd2b4d38dd1
JsOoOng <qkr030503@naver.com>
2026-09-03 20:30:36 +0900

    Update HeadEmployeeService.java

--- kiosk/src/main/java/com/kiosk/headquarter/service/HeadEmployeeService.java
+++ kiosk/src/main/java/com/kiosk/headquarter/service/HeadEmployeeService.java
@@ -112,6 +112,7 @@ public class HeadEmployeeService {
             );
         }
 
+        //test
         // 8. 지점 관리자 엔티티 생성
         Employee employee = Employee.builder()
                 .store(store)
